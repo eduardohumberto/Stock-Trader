92a5a5e5e4ea818473b577601ac3a3cc365d7f46
Eduardo humberto <eduardo.humberto1992@gmail.com>
2017-12-01 15:38:26 -0200

    create modules

---
+++ src/store/modules/stocks.js
@@ -0,0 +1,39 @@
+import stocks from '../../data/stocks';
+
+const state = {
+    stocks: []
+};
+
+const mutations = {
+    setStocks(state,stocks){
+        state.stocks = stocks;
+    },
+    randomStocks(state){
+
+    }
+};
+
+const actions = {
+    buyStock: ( { commit }, order ) => {
+         commit();
+    },
+    initStocks: ( { commit }) => {
+        commit('setStocks', stocks);
+    },
+    randomizeStocks: ( { commit } ) => {
+        commit( 'randomStocks' )
+    }
+};
+
+const getters = {
+    stocks: state => {
+         return state.stocks
+    }
+}
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
